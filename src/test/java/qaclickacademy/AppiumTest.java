8108bd496820245bdb6255f4ec808401a16183c7
Venkatesh <venkateshgalla1996@gmail.com>
2020-08-12 16:16:19 +0530

    First commit

---
+++ src/test/java/qaclickacademy/AppiumTest.java
@@ -0,0 +1,22 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+	
+	@Test
+	public void browserautomation()
+	
+	{
+		System.out.println("Hello");
+	}
+	@Test
+	public void browserautomation1()
+	
+	{
+		System.out.println("Hello1");
+		
+	}
+	
+}
---
+++ src/test/java/qaclickacademy/SeleniumTest.java
@@ -0,0 +1,20 @@
+package qaclickacademy;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest {
+	
+	@Test
+	public void browserautomation()
+	
+	{
+		System.out.println("Hello");
+	}
+	@Test
+	public void browserautomation1()
+	
+	{
+		System.out.println("Hello1");
+	}
+
+}
